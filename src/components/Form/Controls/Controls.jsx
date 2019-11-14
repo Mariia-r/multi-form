3082265f36dea10a17d8c8a349129783fc440fc3
Mariia-r <mariya-r1@ukr.net>
2019-11-14 19:26:53 +0200

    add animation, add image-block, fix submit

--- src/components/Form/Controls/Controls.jsx
+++ src/components/Form/Controls/Controls.jsx
@@ -5,13 +5,15 @@ import "./Controls.css";
 
 const Controls = (props) => {
     const statePages = props.pages.map((page, id) => {
-        return <li key={id} className={`step ${page === props.page && "active"}`}></li>
+        return <li key={id} className={`step ${page === props.page && "step-active"}`}></li>
     })
     
     return (
         <div className="controls">
-                <button className="controls-btn btn-prev" onClick={props.prevPage} disabled={props.page === 1 && true}>
-                    <img src={btnPrev}/>
+                <button className="controls-btn btn-prev" 
+                        onClick={props.prevPage} 
+                        disabled={props.page === 1 && true}>
+                    <img alt="button Previous"src={btnPrev}/>
                 </button>
                 <div>
                     <ul className="steps">
@@ -20,8 +22,8 @@ const Controls = (props) => {
                 </div>
                 <button className="controls-btn btn-next" 
                         onClick={props.nextPage} 
-                        disabled={props.page === 4 || (props.submitPages.indexOf(props.page) === -1) && true}>
-                    <img src={btnNext}/>
+                        disabled={(props.page === 4 || (props.submitPages.indexOf(props.page) === -1)) && true}>
+                    <img alt="button Next" src={btnNext}/>
                 </button>
         </div>
     )
